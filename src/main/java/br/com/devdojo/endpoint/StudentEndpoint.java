3dd979031ef5d132c6750d2e4d7d992185072a1f
cicero_oliverio <cicero_oliverio@hotmail.com>
2020-02-27 21:30:09 -0300

    Aula 7 padroes REST e POST 03

--- src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/endpoint/StudentEndpoint.java
@@ -7,7 +7,12 @@ import java.time.LocalDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,14 +28,14 @@ public class StudentEndpoint {
     this.dateUtil = dateUtil;
   }
 
-  @RequestMapping(method = RequestMethod.GET)
+  @GetMapping
   public ResponseEntity<?> listAll() {
 
     dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now());
     return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
   }
 
-  @RequestMapping(method = RequestMethod.GET, path = "/{id}")
+  @GetMapping(path = "/{id}")
   public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
     Student student = new Student();
     student.setId(id);
@@ -43,5 +48,23 @@ public class StudentEndpoint {
 
   }
 
+  @PostMapping
+  public ResponseEntity<?> save(@RequestBody Student student) {
+    Student.studentList.add(student);
+    return new ResponseEntity<>(student,HttpStatus.OK);
+  }
+
+  @DeleteMapping
+  public ResponseEntity<?> delete(@RequestBody Student student) {
+    Student.studentList.remove(student);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
+
+  @PutMapping
+  public ResponseEntity<?> update(@RequestBody Student student) {
+    Student.studentList.remove(student);
+    Student.studentList.add(student);
+    return new ResponseEntity<>(HttpStatus.OK);
+  }
 
 }
